afecdef83b9b37a1d663e4c491769958fdbc3e68
Andrey Tikhanov <andrei.tihanov@mail.ru>
2024-05-16 13:03:50 +0400

    create class Message

--- DataBase/SQLite/SQLiteMessangerRepository.cs
+++ DataBase/SQLite/SQLiteMessangerRepository.cs
@@ -1,4 +1,21 @@
-﻿namespace Messanger.DataBase.SQLite {
+﻿using Microsoft.Data.Sqlite;
+
+namespace Messanger.DataBase.SQLite {
     internal class SQLiteMessangerRepository : IMessangerRepository {
+        private const string connectionString = "Data Source ";
+        private SqliteConnection _connection;
+        public SQLiteMessangerRepository(string connectionString = connectionString) {
+            _connection = new SqliteConnection(connectionString);
+        }
+
+        public bool CloseConnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool OpenConnect()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
