7338f1e0f0a54be3e474e2397a77ee7ab3f1923a
Chinoachee <djironov@mail.ru>
2024-05-20 13:17:27 +0400

    desing work

--- View/Pages/AutorizationPages/AutorizationPage.xaml.cs
+++ View/Pages/AutorizationPages/AutorizationPage.xaml.cs
@@ -1,11 +1,24 @@
 ﻿using Messanger.View.Pages.AutorizationPages;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Messanger.View {
     public partial class AutorizationPage : Page {
         public AutorizationPage() {
             InitializeComponent();
             RegistrationFrame.NavigationService.Navigate(new LoginPage());
+        }
+
+        private void gotoLoginPage_Click(object sender,System.Windows.RoutedEventArgs e) {
+            gotoLoginPage.BorderBrush = (Brush)new BrushConverter().ConvertFrom("#eeaa74");
+            gotoRegistrationPage.BorderBrush = (Brush)new BrushConverter().ConvertFrom("Transparent");
+            RegistrationFrame.NavigationService.Navigate(new LoginPage());
+        }
+
+        private void gotoRegistrationPage_Click(object sender,System.Windows.RoutedEventArgs e) {
+            gotoLoginPage.BorderBrush = (Brush)new BrushConverter().ConvertFrom("Transparent");
+            gotoRegistrationPage.BorderBrush = (Brush)new BrushConverter().ConvertFrom("#eeaa74");
+            RegistrationFrame.NavigationService.Navigate(new RegisrationPage());
         }
     }
 }
--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -1,14 +1,22 @@
 ﻿using Messanger.DataBase;
 using Messanger.DataBase.MySQL;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Messanger.View.Windows {
     public partial class MainWindow : Window {
         IMessangerRepository _repository;
+        public static MainWindow window;
         public MainWindow() {
             _repository = new MySQLMessangerRepository();
             InitializeComponent();
             mainPage.NavigationService.Navigate(new AutorizationPage());
+            window = this;
+        }
+        private void MovingWindow(object sender,RoutedEventArgs e) {
+            if(Mouse.LeftButton == MouseButtonState.Pressed) {
+                MainWindow.window.DragMove();
+            }
         }
     }
 }
